346715cd980a8ac05f6753934c125e25c451c10b
akash13nov <akash13nov@gmail.com>
2026-01-09 15:03:40 +0530

    Fix routing for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import ProductsContext from "./context/ProductsContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/context-api-project">
     <ProductsContext>
       <App />
     </ProductsContext>
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/context-api-project",
+  base: "/context-api-project/",
 });
